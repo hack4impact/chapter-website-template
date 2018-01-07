fe1a6aa17d739b2e82a8bbad5b6dffebcd20e015
Timothy Ko <timothy.l.ko@gmail.com>
2018-01-06 19:07:02 -0800

    requriements

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':

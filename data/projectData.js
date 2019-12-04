ba06658d49cece5d5adad035a987f4d04ce61437
choiboy98 <daniel.won.choi@gmail.com>
2019-12-04 01:24:10 -0600

    added files for projects

--- data/projectData.js
+++ data/projectData.js
@@ -1,5 +1,5 @@
 import Fa18Data from "./projects/semesters/fa18";
 import Sp18Data from "./projects/semesters/sp18";
 import Sp19Data from "./projects/semesters/sp19";
-
-export default [Sp19Data, Fa18Data, Sp18Data]; //Look at README for format
+import Fa19Data from "./projects/semesters/fa19";
+export default [Fa19Data, Sp19Data, Fa18Data, Sp18Data]; //Look at README for format

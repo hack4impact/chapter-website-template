7ccaf9d9d5c1d72491848f8c5c030e34628ca4f3
choiboy98 <daniel.won.choi@gmail.com>
2019-12-05 04:26:45 -0600

    done

--- data/projects/semesters/fa19/mli.js
+++ data/projects/semesters/fa19/mli.js
@@ -1,99 +1,59 @@
 // Data for the Fa19 YMCA Project
-const name = "YMCA New American Welcome Center";
+const name = "Mehta's Lab at UIC";
 const id = "mli";
 const detail = "How might we create an algorithm that denoises x-ray images and allow for the research lab to better diagnose patients?";
-const coverImagePath = "/static/images/projects/c2tc-2/c2tc-2-cover.jpg";
+const coverImagePath = "/static/images/projects/mli/cover.png";
 const clientDetail =
-  "The New American Welcome Center at the University Y helps immigrants fully integrate into American society and prepares receiving communities around Champaign County to be welcoming and inclusive. By engaging local institutions and mobilizing community resources, we help make our community a place where newcomers and immigrants can thrive and flourish.";
+  "Ankit I. Mehta’s lab at the University of Illinois at Chicago (UIC MLI) specializes in “the surgical treatment of complex spinal conditions including tumors, degenerative and spine diseases”, among which are corrective surgeries which require screws to be placed in patients’ spines to help correct vertebrae alignment. X-rays are taken at the location of any inserted screws at two occasions - one relatively closer to the operation and another later on - so that movement in screws and/or vertebrae can be monitored over time. At each time, two x-rays are taken: one from the patient’s side (lateral view) and another from the back (AP view). In addition to verifying operation success, the “post” x-rays can help diagnose spinal issues that may have occurred as a result of an operation, such as pseudarthrosis.";
 const clientLink =
-  "https://universityymca.org/welcome/";
+  "https://hospital.uillinois.edu/find-a-doctor/ankit-i-mehta";
 const previousLink = null;
-const clientFeatures = [
-  {
-    title: "Students that don't feel safe",
-    detail: "93.9%"
-  },
-  {
-    title: "Never used Safewalks/Saferides",
-    detail: "77.6%"
-  },
-  {
-    title: "Ranks UIUC's safety below 3",
-    detail: "45%"
-  }
-];
+const clientFeatures = null;
 const problem =
-  "Currently, valuable information about Champaign’s resources are scattered and difficult to find. This decentralization is creating an information gap preventing newcomers from reliably accessing resources that they need the most. YMCA has taken steps to centralize this information1, but it remains largely inaccessible due to language limitations and other barriers.";
-const problemImagePath = "/static/images/projects/c2tc-2/c2tc-2-cover.jpg";
+  "For both the lateral and the AP views, there are two separate sources of motion that could contribute to differences between the “pre” and “post” images: movement of the patient and the ovement of the spine and/or screws. The camera itself has preset positions for both views, with millimeter-level noise between separate images that can be effectively ignored. As the movements of the patient are extraneous, they introduce noise that makes it difficult to evaluate patient progress and diagnose any potential issues (e.g. pseudarthrosis) that may cause major health risks. As a result, a patient that may come in after their operation to diagnose why they haven’t seen the expected improvement may require additional x-rays which delay the process, which could result in any medical conditions worsening or potentially another operation being required.";
+const problemImagePath = "/static/images/projects/mli/problem.JPG";
 const solution =
-  "We want to educate students about what’s around them. If we give them the information they need about what’s around campus, they will be better prepared to make decisions on how they should get home. From our research, we discovered that many students value knowing about where emergency phones, open businesses, lighting, past crimes, and other people are. For the app, we focused on giving students information about open businesses, emergency phones, police stations, street lights, bus stops, and past crimes are. ";
-const solutionImagePath = "/static/images/projects/question.jpg";
-const features = [
-  {
-    title: "Filter Data",
-    detail:
-      "Tailor filters based on lifestyle needs including cost preference, languages offered, and location.",
-    imgPath: "/static/images/projects/ymca/customize.png"
-  },
-  {
-    title: "View resource details",
-    detail:
-      "Gain reliable access to information such as required documents and recommended contacts for service providers in the community.",
-    imgPath: "/static/images/projects/ymca/view.png"
-  },
-  {
-    title: "Upload New Resources",
-    detail:
-      "Administrator users can build on the resource base by editing existing and uploading new community resources.",
-    imgPath: "/static/images/projects/ymca/upload.png"
-  }
-];
+  "We will create an algorithm enabling users to upload both the “pre” and “post” images, realign/manipulate them, and eliminate noise induced by movement of the patient. By doing so, the lab will be able to make more accurate diagnoses and prognoses of their patients. Having specific, quantitative data on the relative positions and movements of screws and vertebral bodies of interest would help patients to make the best of their own and the lab’s time and diagnose any complications earlier for better health and less cost to the patient.";
+const solutionImagePath = "/static/images/projects/mli/solution.png";
+const features = null;
 
 const featureImgSize = "7";
-const techStack = ["reactnative", "flask", "mongodb"];
+const techStack = ["python"];
 const quote = null;
 const quoteSource = null;
 const quoteSourceTitle = null;
-const githubLink = "https://github.com/hack4impact-uiuc/c2tc-fall-2018";
+const githubLink = "https://github.com/hack4impact-uiuc/life-after-hate";
 const team = {
   type: "pics",
   detail: [
     {
-      name: "Annie Wu",
+      name: "Arpan Laha",
       role: "Product Manager"
     },
     {
-      name: "Neeraj Aggarwal",
+      name: "Aryn Harmon",
       role: "Tech Lead"
     },
     {
-      name: "Skyler Shi",
+      name: "Eugenia Chen",
       role: "Software Developer"
     },
     {
-      name: "Jacqueline Osborn",
+      name: "Melody Felix",
       role: "Software Developer"
     },
     {
-      name: "Kendall Hester",
+      name: "Michael Chen",
       role: "Software Developer"
     },
     {
-      name: "Anooj Lal",
+      name: "Prashant Pokhriyal",
       role: "Software Developer"
     },
     {
-        name: "Ashwin Saxena",
+        name: "Sabelle Huang",
         role: "Software Developer"
-    },
-    {
-      name: "Chloe Chan",
-      role: "Product Designer"
-    },
-    {
-        name: "Tiffany Duong",
-        role: "Product Researcher"
-    },
+    }
   ]
 };
 

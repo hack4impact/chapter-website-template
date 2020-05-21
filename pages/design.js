e9e7974c171fd4a0575876c128fdaab14409f8a9
Timothy Ko <timothy.l.ko@gmail.com>
2020-05-21 18:18:53 -0500

    redirects /design to bridge (#25)

---
+++ pages/design.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Router from "next/router";
+
+class Design extends React.Component {
+  componentDidMount() {
+    window.location.href = "https://bridge-ui.now.sh";
+  }
+  render() {
+    return <></>;
+  }
+}
+
+export default Design;

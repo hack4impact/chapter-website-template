13c65c1fe7f6404d967227233f301c62adf9315d
Holben888 <bholmesdev@gmail.com>
2021-01-07 22:16:37 -0500

    feat: remove unneeded pages from nav

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,35 +1,24 @@
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import {
-  Navbar,
-  Container,
-  NavbarBrand,
-  NavbarToggler,
-  Collapse,
-  NavItem,
-  Nav,
-} from "reactstrap";
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Navbar, Container, NavbarBrand, NavbarToggler, Collapse, NavItem, Nav } from 'reactstrap';
 
-export default function NavigationBar({ navType }) {
+function NavigationBar({ navType }) {
   const [isTop, setIsTop] = useState(true);
   const [collapsed, setCollapsed] = useState(true);
 
   useEffect(() => {
     const onScroll = () => setIsTop(window.scrollY < 100);
-    document.addEventListener("scroll", onScroll);
+    document.addEventListener('scroll', onScroll);
 
-    return () => window.removeEventListener("scroll", onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
   }, []);
 
   const toggleNavbar = () => setCollapsed(!collapsed);
 
   return (
     <Navbar
-      className={`navbar-expand-lg navbar-dark fixed-top ${
-        isTop ? "" : "shadow bg-white"
-      }`}
-      id={navType ?? "mainNav"}
-    >
+      className={`navbar-expand-lg navbar-dark fixed-top ${isTop ? '' : 'shadow bg-white'}`}
+      id={navType ?? 'mainNav'}>
       <Container>
         <NavbarBrand className="js-scroll-trigger" href="/">
           <img
@@ -54,19 +43,6 @@ export default function NavigationBar({ navType }) {
               </Link>
             </NavItem>
             <NavItem>
-              <Link href="/design">
-                <a className="nav-link pl-3">Design</a>
-              </Link>
-            </NavItem>
-            <NavItem>
-              <a
-                href="https://medium.com/hack4impact-uiuc"
-                className="nav-link pl-3"
-              >
-                Blog
-              </a>
-            </NavItem>
-            <NavItem>
               <Link href="/apply">
                 <a className="nav-link pl-3">Apply</a>
               </Link>
@@ -82,3 +58,5 @@ export default function NavigationBar({ navType }) {
     </Navbar>
   );
 }
+
+export default NavigationBar;

3d0727fb40171b02d89e73b483a8f88e0d6b91b2
Holben888 <bholmesdev@gmail.com>
2021-01-13 21:32:33 -0500

    feat: use homepage "involve" section on apply page

--- pages/apply/index.jsx
+++ pages/apply/index.jsx
@@ -1,55 +1,24 @@
 import React from 'react';
 import GradientBanner from '../../components/gradientBanner';
-import ActionButton from '../../components/actionButton';
 import Head from '../../components/head';
-import { Container, Row, Col } from 'reactstrap';
+import { Container } from 'reactstrap';
+import InvolveSection from '../../components/involveSection';
+import Section from '../../components/section';
 
 function ApplyPage() {
   return (
     <>
       <Head title="Apply" />
-      <GradientBanner title="Apply" arrow />
-      <section className="timeline-split-apply">
+      <GradientBanner
+        title="Apply"
+        subHeadline="We're open to students and nonprofits of all backgrounds, no matter your project or technical expertise."
+        arrow
+      />
+      <Section>
         <Container>
-          <Row>
-            <Col md="6" className="text-center">
-              <a href="https://h4i-project-timeline.now.sh/">
-                <img
-                  src="/images/timeline.jpg"
-                  className="img-fluid"
-                  alt="Project timeline - weeks 1-4: project scoping, weeks 5-12: development, week 13: MVP, weeks 14-18: development, week 19: handoff"
-                />
-              </a>
-            </Col>
-
-            <Col md="6">
-              <Row className="margin-lg-top">
-                <div className="col-md-8 offset-md-3">
-                  <h4 className="text-title">Students</h4>
-                  <p>
-                    Want to apply your skills to benefit the community? Learn how you can build
-                    software products that has potential to impact lives alongside a team of
-                    motivated and talented peers.
-                  </p>
-                  <ActionButton link="/apply/students">Apply</ActionButton>
-                </div>
-              </Row>
-
-              <Row className="margin-lg-top">
-                <div className="col-md-8 offset-md-3">
-                  <h4 clas="text-title">Nonprofits</h4>
-                  <p>
-                    Have a core need that your nonprofit wants solved? Learn more about our services
-                    and how your organization can partner with us to support your organization's
-                    goals and mission.
-                  </p>
-                  <ActionButton link="/apply/nonprofits">Apply</ActionButton>
-                </div>
-              </Row>
-            </Col>
-          </Row>
+          <InvolveSection />
         </Container>
-      </section>
+      </Section>
     </>
   );
 }
